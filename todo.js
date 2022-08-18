509775906858320725a83ba52503d717216e56dc
reddykavya <kavyareddyasi@gmail.com>
2022-08-18 20:13:40 +0530

    first comit

--- todo.js
+++ todo.js
@@ -1,160 +1,161 @@
-showtask();
-var addtaskinput = document.getElementById('addtaskinput')
-let addtaskbtn = document.getElementById("addtaskbtn");
+let arr = [];
 
-addtaskbtn.addEventListener("click", function(e){
-    addtaskinputval = addtaskinput.value; 
-    e.preventDefault();
-    const fetchData = async () => {
-        await fetch("https://jsonplaceholder.typicode.com/users")
-          .then((response) => response.json())
-          .then((data) => console.log(data));
-      };
+function addItem(value){
 
-var obj = {
-    title : document.getElementById('addtaskinput').value
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+    method: 'POST',
+    body: JSON.stringify({
+      title: value,
+      completed:false
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  })
+    .then((response) => response.json())
+    .then((json) => {
+        arr.push(json);
+        renderList(arr);
+    });
+
 }
 
-fetch('https://jsonplaceholder.typicode.com/users', {
-  method: 'POST',
-  body: JSON.stringify(obj),
+function updateItem(item){
+    fetch(`https://jsonplaceholder.typicode.com/posts/${item.id}`, {
+  method: 'PUT',
+  body: JSON.stringify(item),
   headers: {
     'Content-type': 'application/json; charset=UTF-8',
   },
 })
   .then((response) => response.json())
   .then((json) => console.log(json));
+}
+
+fetch('https://jsonplaceholder.typicode.com/posts')
+  .then((response) => response.json())
+  .then((json) =>{
+      arr = json;
+      renderList(arr);
+      console.log(arr)
+  });
+
+let addBtn= document.getElementById('add');
+addBtn.addEventListener('click', add);
 
 
-  //============================================================
-
-    if(addtaskinputval.length == 0){
-        alert("enter task details")
+function add(){
+    const inputElement =  document.getElementById('todoInputname')
+    let inputVal = inputElement.value;
+    const isDuplicate = arr.filter((item)=>{
+      if(item.title === inputVal){
+          return item
+      }
+    });
+    if(isDuplicate.length > 0){
+      alert("duplicate item is trying to add")
+    }else{
+        addItem(inputVal);
     }
+    
+    inputElement.value = ""
+    renderList(arr);
+}
+function renderList(taskarr){
+    let list = ``;
+   
+    taskarr.forEach((arr)=>{
+        list = list+`<tr>
+        <td data-id="${arr.id}" class="${arr.completed ? "marked":"notmarked"}">${arr.title}</td> 
+        <td> <button type="button" data-id="${arr.id}"  class="edit">Edit</button></td>
+        <td> <button type="button" data-id="${arr.id}" id="add" class="delete">Delete</button></td>
+        </tr>`
+    })
+     
+    document.getElementById('todolist').innerHTML = list
+}
+renderList(arr);
 
-    if(addtaskinputval.trim()!=0){
-        taskObj = [];
-        let webtask = localStorage.getItem("localtask");
-        if(webtask == null){
-            taskObj = [];
+document.body.addEventListener('click',(e)=>{
+    if(e.target.tagName === "TD"){
+        const id = e.target.getAttribute("data-id");
+
+        arr = arr.map((item)=>{
+            if(item.id == id){
+                item.completed = true;
+                return item
+            }else{
+                return item
+            }
+
+        });
+        renderList(arr);
+
+    }else if(e.target.tagName === "BUTTON"){
+        const id = e.target.getAttribute("data-id");
+        let currentUpdateItem = {} 
+        if(e.target.innerHTML === "Edit"){
+            const editItem = arr.filter((item)=>{
+                if(item.id == id){
+                    currentUpdateItem = item;
+                    return item;
+                } 
+            });
+            const editVal = editItem[0].title;
+             
+            const inputContainer =  document.getElementById("inputContainer")
+
+            inputContainer.innerHTML=`
+            <input type="text" class="todo-user-input" value="${editVal}" placeholder="What needs to be add?" id="todoUpdatename" />
+                <button type="button" id="update"  class="list-button">
+                   Save Task
+                </button>
+            `;
+
+    const todoUpdatename = document.getElementById("todoUpdatename");
+    const updateBtn = document.getElementById('update');
+    
+    updateBtn.addEventListener('click',function(){
+        currentUpdateItem.title = todoUpdatename.value;
+        updateItem(currentUpdateItem);
+      
+        inputContainer.innerHTML=`<input type="text" class="todo-user-input" placeholder="What needs to be add?" id="todoInputname" />
+        <button type="button" id="add"  class="list-button">
+           Add Task
+        </button>`;
+        addBtn= document.getElementById('add');
+        addBtn.addEventListener('click', add);
+    });
+
+          }
+        else if(e.target.innerHTML === "Delete"){
+            fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+                    method: 'DELETE'});
+            arr = arr.filter((item) => {
+                
+                if (item.id == id){
+                  
+                }else{
+                    return item
+                }
+            });
         }
-        else{
-            taskObj = JSON.parse(webtask);   
-        }
 
-     let found = taskObj.filter(item => item.task_name == addtaskinputval);
-     if(found.length == 0){
-        taskObj.push({'task_name':addtaskinputval, 'completeStatus':true});
-        localStorage.setItem("localtask", JSON.stringify(taskObj));
-        addtaskinput.value = '';
-     }
-     else{   
-        addtaskinput.value = '';
-     }
-        console.log(taskObj)
-    }
-    showtask();
-})
-
-// showtask
-function showtask(){
-    let addtaskinput = document.getElementById("addtaskinput");
-    let webtask = localStorage.getItem("localtask");
-     addtaskinputval = addtaskinput.value;
-    if(webtask == null){
-        taskObj = [];
+        renderList(arr)
     }
     else{
-        taskObj = JSON.parse(webtask);
+        console.log("not clicked on td")
     }
-    let html = '';
-    let addedtasklist = document.getElementById("addedtasklist");
-
-    taskObj.forEach((item, index) => {
-
-        if(item.completeStatus==true){
-            taskCompleteValue = `<td class="completed">${item.task_name}</td>`;
-            
-        }else{
-          
-            taskCompleteValue = `<td>${item.task_name}</td>`;
-        }
-        html += `<tr>
-                    <th>${index+1}</th>
-                    ${taskCompleteValue}
-                    <td><button onclick="edittask(${index})" class="edit"><i class="fa fa-edit"></i></button></td>
-                    <td><button onclick="deleteitem(${index})" class="delete"><i class="fa fa-trash"></i></button></td>
-                </tr>`;
-    });
-    
-    addedtasklist.innerHTML = html;
-    
-}
-
-// edittask
-function edittask(index){
-   var obj1 = {
-    title : document.getElementById('addtaskinput').value
-}
-    let saveindex = document.getElementById("saveindex");
-    let addtaskbtn = document.getElementById("addtaskbtn");
-    let savetaskbtn = document.getElementById("savetaskbtn");
-    saveindex.value = index;
-    let webtask = localStorage.getItem("localtask");
-    var taskObj = JSON.parse(webtask); 
-    
-    addtaskinput.value = taskObj[index]['task_name'];
-    addtaskbtn.style.display="none";
-    savetaskbtn.style.display="block";
-    
-}
-
-// savetask
-let savetaskbtn = document.getElementById("savetaskbtn");
-savetaskbtn.addEventListener("click", function(){
-    let addtaskbtn = document.getElementById("addtaskbtn");
-    let webtask = localStorage.getItem("localtask");
-    let taskObj = JSON.parse(webtask); 
-    let saveindex = document.getElementById("saveindex").value;
-    
-    for (keys in taskObj[saveindex]) {
-        if(keys == 'task_name'){
-            taskObj[saveindex].task_name = addtaskinput.value;
-            fetch('http://localhost:3000/posts/3', {
-  method: 'PUT',
-  body: JSON.stringify(taskObj[saveindex]),
-  headers: {
-    'Content-type': 'application/json; charset=UTF-8',
-  },
 })
-  .then((response) => response.json())
-  .then((json) => console.log(json));
-        }
-      }
-  
-    savetaskbtn.style.display="none";
-    addtaskbtn.style.display="block";
-    localStorage.setItem("localtask", JSON.stringify(taskObj));
-    addtaskinput.value='';
-    showtask();
-})
-// deleteitem
-function deleteitem(index){
-
-    let webtask = localStorage.getItem("localtask");
-    let taskObj = JSON.parse(webtask);
-    taskObj.splice(index, 1);
-    localStorage.setItem("localtask", JSON.stringify(taskObj));
-    
-    let options = {
-        method:"DELETE"
-    }
-    fetch('http://localhost:3000/posts/1',options)
-    .then(response =>console.log(response.status));
-    showtask();
-}
 
 
+
+
+
+
+
+
+ 
 
 
 
